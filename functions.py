1f16d9087f30e502733416318632cc287762a3bc
Sterrrrest <150155969+Sterrrrest@users.noreply.github.com>
2024-05-01 12:47:07 +0300

    Add project

---
+++ functions.py
@@ -0,0 +1,61 @@
+import requests
+import os
+
+from environs import Env
+from bs4 import BeautifulSoup
+from pathlib import Path
+from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlparse
+
+env = Env()
+env.read_env()
+location = env('PATH')
+
+
+def download_txt(response, filename, folder='books/'):
+    path = f"{folder}"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    filename = os.path.join(f'{folder}{sanitize_filename(filename)}.txt')
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+    return filename
+
+
+def download_image(img_url, filename, folder='foto/'):
+
+    Path(f"{folder}").mkdir(parents=True, exist_ok=True)
+    response = requests.get(img_url)
+    response.raise_for_status()
+
+    with open(f"{folder}{filename}", 'wb') as file:
+        file.write(response.content)
+
+
+def parse_book_page(page):
+    page_soup = BeautifulSoup(page.text, 'lxml')
+
+    img_tag = page_soup.find('div', class_='bookimage').find('img')['src']
+    book_img_url = urljoin('https://tululu.org', img_tag)
+    filename_img = sanitize_filename(urlparse(book_img_url).path.split('/')[-1])
+
+    type_books_tags = page_soup.find('span', class_='d_book').find_all('a')
+    type_books = []
+    for t in type_books_tags:
+        type_books.append(t.text)
+
+    comments_tag = page_soup.find_all('div', class_='texts')
+    comments = []
+    for comment in comments_tag:
+        comments.append(comment.text.split(')')[-1])
+
+    book = {'title': page_soup.find('h1').text.split('::')[0].strip(),
+            'author': page_soup.find('h1').text.split('::')[1].strip(),
+            'img_url': book_img_url,
+            'filename_img': filename_img,
+            'types_book': type_books,
+            'comments': comments}
+
+    return book
+
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import requests
-import os
-from pathlib import Path
+import argparse
 
-path = "/Users/sterr/Documents/Python/Books/books"
-Path(path).mkdir(parents=True, exist_ok=True)
+from functions import download_txt, download_image, parse_book_page
 
-for i in range(10):
-    url = f'https://tululu.org/txt.php'
-    payLoad = {'id': i+1}
-    response = requests.get(url, params=payLoad)
+
+parser = argparse.ArgumentParser(
+    description='Скачивает книги в заданном диапазоне'
+)
+parser.add_argument('-f', '--first_book', help='Номер первой книги', type=int, default=1)
+parser.add_argument('-l', '--last_book', help='Номер последней книги', type=int, default=10)
+args = parser.parse_args()
+
+for id_book in range(args.first_book, args.last_book):
+    url_download = f"https://tululu.org/txt.php?id={id_book}"
+    response = requests.get(url_download)
     response.raise_for_status()
+
     if not response.history:
-        filename = f'books/id{i}.txt'
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+
+        url_name = f'https://tululu.org/b{id_book}'
+        book_page = requests.get(url_name)
+        book_page.raise_for_status()
+        filename = f"{id_book}. {parse_book_page(book_page)['title']}"
+        download_txt(response, f"{id_book}. {parse_book_page(book_page)['title']}", folder='books/')
+
+        download_image(parse_book_page(book_page)['img_url'],
+                       (parse_book_page(book_page)['filename_img']), folder='foto/')
+
+        comments = parse_book_page(book_page)['comments']
+
+        type_books = parse_book_page(book_page)['types_book']
+
